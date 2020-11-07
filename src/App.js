4de3aae27bd33a5984e3ace03b026e1ba0903e03
Indi Boteju <indiboteju@Indis-MBP.lan>
2020-11-06 16:50:17 -0800

    profile view

--- src/App.js
+++ src/App.js
@@ -7,8 +7,34 @@ import ManyPuppies from './components/ManyPuppies';
 import ProfilePreview from './components/ProfileView';
 
 function App() {
+
+  const profileDatefromDb = [
+    {    
+    username: "James",
+    thumbnailUrl:'https://images-cdn.9gag.com/photo/aOKgZpR_460s.jpg',
+    likeCount: 1,
+    bio: "this is my bio."
+    },
+    {    
+    username: "Queen",
+    thumbnailUrl:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNjIzMzUxODIx/queen-elizabeth-ii-9286165-1-402.jpg',
+    likeCount: 1,
+    bio: "this is my bio."
+  },
+  {    
+  username: "Nice guy",
+  thumbnailUrl:'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=899&h=471&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2016%2F04%2Fdkalfdsdada.jpg',
+  likeCount: 55,
+  bio: "this is my bio."
+  },
+
+
+
+
+  ]
   return (
-    <div className="App">
+    <div className="App" style={{width: "60%", 
+    margin: "0 auto" }} >
       <h1> Hello from Main Page Navigation!</h1>
       <Router >
         <PuppiesComp path="/puppy"/> 
@@ -16,12 +42,24 @@ function App() {
         <ManyPuppies path="/:number" />
       </Router>
 
-      <ProfilePreview 
+      {/* <ProfilePreview 
       username="James"
       thumbnailUrl={'https://images-cdn.9gag.com/photo/aOKgZpR_460s.jpg'}
       likeCount={3}
       bio="this is my bio."      
-      />
+      /> */}
+
+      {profileDatefromDb.map((profileData, i) => {
+        return <ProfilePreview key={i} 
+        data={profileData}
+        // username={profileData.username}
+        // thumbnailUrl={profileData.thumbnailUrl}
+         
+        />
+      })
+      }
+
+  
     </div>
   );
 }
--- src/components/ProfileView.js
+++ src/components/ProfileView.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState}  from 'react';
 
 const ProfilePreview = (props) => {
-
-//const { username, likes, thumbnailUrl, bio} = props;
+    const [likes, setLikes] = useState(props.data.likeCount)
+    console.log(props);
+    const { username, likeCount, thumbnailUrl, bio} = props.data;
 // so the props doesn need to be repeated below;
 
-return (
-    <div>
-        <h2>
-            {props.username}
-        </h2>
-        <p>Likes: {props.likeCount}</p>
-        <img src={props.thumbnailUrl} alt="Thumbnail" />
-        <h4>Bio</h4>
-        <p>{props.bio}</p>
-    </div>
-)
-
+const containerStyle = {
+    borderRadius:5,
+    padding: 50,
+    border: "1px solid gray",
+    marginBottom: 20,
+    boxShadow: "10px 5px 5px darkgray",
+    maxWidth: 250
 }
+
+const imgStyle = {
+    borderRadius: "200px",
+    boxShadow: "10px 5px 5px gray"
+}
+
+    return (
+        <div style={containerStyle}>
+            <h2>
+                {username}
+            </h2>
+            <p>
+                <span role="img" aria-label="Thumbs up"
+                 onClick={(event) => {
+                     props.data.likeCount++}}
+                
+                >  &#128077;</span>
+                 {likeCount}</p>
+            <img style={imgStyle} width="100%" src={thumbnailUrl} alt="Thumbnail" />
+            <h4>Bio</h4>
+            <p>{bio}</p>
+        </div>
+    )
+
+    }
 export default ProfilePreview;
